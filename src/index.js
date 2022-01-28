6c2e2241cd23c46ae7ddde3ad4ecb87b8e219821
markadiusch <hudzik.m@icloud.com>
2022-01-28 21:49:44 +0100

    a

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 "use strict"
-function fetchMovies(){
+export function fetchMovies(){
     return;
 }
--- src/services.js
+++ src/services.js
@@ -1,4 +1,4 @@
 "use strict"
-function fetchWithTimeout(delay){
+export function fetchWithTimeout(delay){
     return new Promise(resolve => setTimeout(resolve, delay));
 }
