c14393b14c7377926729dd13e6508d156067b36a
markadiusch <hudzik.m@icloud.com>
2022-01-29 11:00:21 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -29,8 +29,14 @@ getBooksOrMoviesPromise.then(results => {
 
 async function getBooksAndMoviesAsync(){
     try {
-        const [books, movies] = await Promise.all([asyncFetchBooks, asyncFetchMovies]);
-        return {books, movies};
+        const [books, movies] = await Promise.all([
+            asyncFetchBooks,
+            asyncFetchMovies
+        ]);
+        return {
+            books,
+            movies
+        };
     } catch (error) {
         console.log("Eroor fetching books and movies", error);
     }
@@ -38,15 +44,19 @@ async function getBooksAndMoviesAsync(){
 
 async function getBooksOrMoviesAsync(){
     try {
-        const values = await Promise.race([asyncFetchBooks(), asyncFetchMovies()]);
+        const values = await Promise.race([
+            asyncFetchBooks(),
+            asyncFetchMovies()
+        ]);
         return values;
     } catch (error) {
         console.error("Error waiting for the promise race", error);
-         
+        throw error; 
     }
 }
 
-getBooksAndMoviesAsync().then(results =>{
+getBooksAndMoviesAsync()
+    .then(results => {
     console.log("movies and books", {
         movies: results.movies,
         books: results.books
