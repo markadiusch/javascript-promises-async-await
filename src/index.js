a8f03ef212876992533e7b5621d61e77809df05d
markadiusch <hudzik.m@icloud.com>
2022-01-29 10:56:26 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -38,7 +38,7 @@ async function getBooksAndMoviesAsync(){
 
 async function getBooksOrMoviesAsync(){
     try {
-        const values = await Promise.race([asyncFetchBooks(), asyncFetchMovies]);
+        const values = await Promise.race([asyncFetchBooks(), asyncFetchMovies()]);
         return values;
     } catch (error) {
         console.error("Error waiting for the promise race", error);
